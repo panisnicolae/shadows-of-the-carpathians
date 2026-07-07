5fdd208575d7fa813cefe3b4869b562e6f03795e
Nicolae Panis <nicolae.panis@student.tuiasi.ro>
2026-07-07 12:38:19 +0300

    added main menu button to GameOverScreen

--- core/src/main/kotlin/io/github/feykro/sotc/screens/GameOverScreen.kt
+++ core/src/main/kotlin/io/github/feykro/sotc/screens/GameOverScreen.kt
@@ -1,33 +1,82 @@
 package io.github.feykro.sotc.screens
 
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator
+import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
 import com.badlogic.gdx.utils.ScreenUtils
+import com.badlogic.gdx.utils.viewport.ScreenViewport
 import io.github.feykro.sotc.MainGame
 
 class GameOverScreen(game: MainGame) : BaseScreen(game) {
 
-    private val font = BitmapFont()
+    private val stage = Stage(ScreenViewport())
+    private val skin = Skin(Gdx.files.internal("metal/metal-ui.json"))
+    private val table = Table()
+
+    private lateinit var font: BitmapFont
+    private lateinit var title: Label
+
+    override fun show() {
+        Gdx.input.inputProcessor = stage
+
+        val generator = FreeTypeFontGenerator(Gdx.files.internal("LaughTalesDemo.otf"))
+        val parameter = FreeTypeFontGenerator.FreeTypeFontParameter().apply {
+            size = 48
+            color = Color.WHITE
+        }
+
+        font = generator.generateFont(parameter)
+        generator.dispose()
+
+        title = Label("GAME OVER", Label.LabelStyle(font, Color.RED))
+
+        val menuButton = TextButton("Main Menu", skin)
+        menuButton.label.setFontScale(2f)
+
+        menuButton.addListener(object : ClickListener() {
+            override fun clicked(event: InputEvent?, x: Float, y: Float) {
+                game.setScreen(MainMenuScreen(game))
+            }
+        })
+
+        table.setFillParent(true)
+        table.defaults().pad(20f)
+
+        table.add(title)
+        table.row()
+        table.add(menuButton)
+            .width(250f)
+            .height(80f)
+
+        stage.addActor(table)
+    }
 
     override fun render(delta: Float) {
-        ScreenUtils.clear(Color.BLACK)
+        ScreenUtils.clear(0f, 0f, 0f, 1f)
 
-        camera.update()
-        game.batch.projectionMatrix = camera.combined
+        stage.act(delta)
+        stage.draw()
+    }
 
-        game.batch.begin()
-        font.draw(game.batch, "GAME OVER", 120f, 110f)
-        font.draw(game.batch, "Press ENTER to restart", 80f, 90f)
-        game.batch.end()
+    override fun resize(width: Int, height: Int) {
+        stage.viewport.update(width, height, true)
+    }
 
-        if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
-            game.screen = GameScreen(game)
-        }
+    override fun hide() {
+        Gdx.input.inputProcessor = null
     }
 
     override fun dispose() {
+        stage.dispose()
+        skin.dispose()
         font.dispose()
     }
 }
